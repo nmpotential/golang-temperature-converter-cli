4e044111c7cfcd5630b7dca4c3198d1763755295
nmpotential <nathan.mokonz@gmail.com>
2020-12-04 09:08:22 +0000

    Added annotations to main.go

--- README.md
+++ README.md
@@ -1,51 +1,7 @@
-# Project Objectives
-
-In this project, we built a command line application which has the functionality to parse 
-CLI arguments and reads keyboard inputs from the terminal in Go.
-
-## How to: Build a CLI app that parses CLI arguments and reads keyboard inputs from the terminal in Go
 
 ### 1. Declare variables needed for expected input and expected/unexpected output
-- [ ] Outline how the app supposed to work and put in README.md(this will be updated as 
-      functionality and implementation are refined):
-
-- [ ] What are expected inputs:
-
-- [ ] What are expected outputs:
-
-- [ ] Finally, declare variables needed for expected/unexpected input and expected/unexpected 
-output (Hint: Think about what we need in between input -> output):
-
-### Example: Temperature Converter CLI
-
-- [x] Outline how the apps supposed to work:
-        - *User enters value and unit of temperature and then app prints out converted temperature 
-between Fahrenheit and Celsius.*
-
-- [x] What are the expected inputs:
-        - Value and unit of temperature
-        
-- [x] What are the expected outputs of the app:
-        - Value of temperature in converted unit 
 
 
-
-- [x] Finally, declare variables needed for expected/unexpected input and expected/unexpected 
-output (Hint: Think about what we need in between input -> output):
-
-```
-package main
-
-var originUnit string	// Expected Input
-var originValue float64	// Input
-
-var shouldConvertAgain string //Output
-
-var err error //Error output
-
-var errInvalidArguments = errors.New("Invalid arguments")	// Error output for invalid arguments
-var errReadingInput = errors.New("Error reading input")	// Error output for issue with reading input
-```
 
 ### 2. Create methods for parsing CLI arguments and reading keyboard inputs from the terminal 
 - Take methods and find out what its required to create these methods
@@ -54,12 +10,12 @@ var errReadingInput = errors.New("Error reading input")	// Error output for issu
 
 ###   2.1. Create method for parsing CLI arguments 
 - In the main func (or otherwise) validate arguments by check if the length of args 
-is different than 2. If so, invoke the printError() function, passing errInvalidArguments
+is different from10.  2. If so, invoke the printError() function, passing errInvalidArguments
 as the argument.
         
 #   2.2. Create method for reading keyboard input
 - In the main func (or otherwise) validate arguments by check if the length of args 
-is different than 2. If so, invoke the printError() function, passing errInvalidArguments
+is different from 2. If so, invoke the printError() function, passing errInvalidArguments
 as the argument.
 
 
@@ -71,7 +27,7 @@ test for reading input
 
 Compile with `go build -o temp`.
 
-Then, invoke the binary passing as argument the unit of temperature we want to convert **from**.
+Then, invoke the binary passing as argument the unit of temperatdure we want to convert **from**.
 For example:
 
 `./temp C` to convert from Celsius to Fahrenheit or `./temp F` to convert from Fahrenheit to Celsius.
--- main.go
+++ main.go
@@ -6,99 +6,89 @@ import (
 	"os"
 	"strings"
 )
+//3. Declare variables
+var originUnit string   // Input: The arguments(s) that will be parsed into the CLI
+var originValue float64 // Input: The keyboard input that will be read from the terminal
 
-var originUnit string   // Expected Input
-var originValue float64 // Expected Input
+var shouldConvertAgain string // Input: The input functionality that enables the app to either restart or end
 
-var shouldConvertAgain string //output
+var err error //Output: The error responsible for handling all other potential errors the app may encounter
 
-var err error //error output
-
-var errInvalidArguments = errors.New("Invalid arguments") //error output for invalid arguments
-var errReadingInput = errors.New("Error reading input")   //error output for issue with reading input
+var errInvalidArguments = errors.New("Invalid arguments") // Output: The error responsible for handling invalid arguments(s) that will be parsed into the CLI
+var errReadingInput = errors.New("Error reading input")   // Output: The error responsible for handling issues with keyboard input that will be read from the terminal
 
 func main() {
-	/* Validate arguments
-	• Check if the length of args is different than 2.
-	• If so, invoke the printError() function, passing
-	errInvalidArguments as the argument.
-	*/
+	//4. Validate Argument(s)
+	// We use the built-in len function to check if the length of os.Args is different than 2.
 	if len(os.Args) != 2 {
+		// If argument invalid we invoke the printError() function, passing errInvalidArguments as the argument.
 		printError(errInvalidArguments)
 	}
-	/* Read origin unit
-	• Invoke the strings.ToUpper() function passing os.Args[1] as the argument.
-	This ensures consistency when reading command line arguments provided by the user.
-	• Assign the result to the previously defined originUnit variable.
-	*/
+	//5. Read origin unit
+	// Invoke strings.ToUpper function to ensure consistency when reading command line
+	// originUnit value provided by the user
+	// originUnit is the 2nd argument, os.Args[1], parsed into CLI
+	// the executable is 1st argument , os.Args[0], by default
 	originUnit := strings.ToUpper(os.Args[1])
-
+	//6. Read current temperature (originValue) Keyboard Input
 	for {
+		// Print statement that will prompt user to enter current temperature
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
-		/* Read current temperature
-		• Invoke the fmt.Scanln() function, passing &originValue as the argument.
-		• Assign the two return values to the variables _ and err respectively.
-		*/
+		// Invoke the fmt.Scanln() function, passing &originValue as the argument.
+		// Assign the two return values to the variables _ and err respectively.
 		_, err := fmt.Scanln(&originValue)
-		/*
-			• Create an if statement checking if err != nil, and if that's true,
-			invoke the printError() function, passing errReadingInput as its argument.
-		*/
+ 		// If statement checking if err != nil, and if that's true,
+ 		// invoke the printError() function, passing errReadingInput as its argument.
 		if err != nil {
 			printError(errReadingInput)
 		}
-		/* Convert the temperature
-		• Create an if statement to check if originUnit is equal to "C"
-		(notice the capital casing).
-		• If so, invoke the convertToFahrenheit()
-		function passing originValue as the argument.
-		• Otherwise, create an else block and invoke the convertToCelsius() function,
-		passing originValue as the argument.
-		*/
+		//9. Generate methods that will utilise core functionality functions
+
+		// If statement to check if originUnit is equal to "C" (notice the capital casing).
+		// If so, invoke the convertToFahrenheit() function passing originValue as the argument.
 		if originUnit == "C" {
+			// Convert current temperature from Celsius to Fahrenheit
 			convertToFahrenheit(originValue)
+			// Otherwise, create an else block and invoke the convertToCelsius() function,
+			// passing originValue as the argument.
 		} else {
+			// Convert current temperature from Fahrenheit to Celsius
 			convertToCelsius(originValue)
 		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
-		/* Prompt to convert again
-		• Invoke the fmt.Scanln() function passing &shouldConvertAgain as its argument.
-		• Assign the two return values to the previously defined variables _ and err respectively.
-		• On the following line, create an if statement checking if err != nil.
-		• If that condition is true, invoke printError() passing errReadingInput as its argument.
-		*/
+		//10. Prompt to convert again
+		// Invoke the fmt.Scanln() function passing &shouldConvertAgain as its argument.
+		// Assign the two return values to the previously defined variables _ and err respectively.
 		_, err = fmt.Scanln(&shouldConvertAgain)
+		// On the following line, create an if statement checking if err != nil.
+		// If that condition is true, invoke printError() passing errReadingInput as its argument.
 		if err != nil {
 			printError(errReadingInput)
 		}
-		/* Parse prompt answer
-		• Currently, we can't always be sure that the value assigned to shouldConvertAgain is
-		going to be in the casing we expect.
-		• Let's fix this by making a small change to the condition on the last if statement
-		inside the for loop.
-		• The condition should be a combination of calling the strings.ToUpper() function,
-		passing it the result of calling the strings.TrimSpace() function with shouldConvertAgain
-		as its argument.
-		•If the result of all of that is NOT equal to "Y", then the existing if block containing
-		fmt.Println("Good bye!") followed by break should be run.
-		*/
+		//11. Parse prompt answer
+		// To ensure consistency when reading command line, we invoke a combination
+		// of strings.ToUpper function passing it the result of calling the
+		// strings.TrimSpace() function with shouldConvertAgain as its argument.
+		// If result is NOT equal to "Y", then the existing if block containing then "Good bye!" followed by break
 		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
 	}
 }
-
+//7. Create function that handles runtime errors
 func printError(err error) {
 	fmt.Fprintf(os.Stderr, "error: %v\n", err)
 	os.Exit(1)
 }
 
+// 8. State each core functionality of app and create functions for each
+// Convert current temperature from Fahrenheit to Celsius
 func convertToCelsius(value float64) {
 	convertedValue := (value - 32) * 5 / 9
 	fmt.Printf("%v F = %.0f C\n", value, convertedValue)
 }
-
+// Convert current temperature from Celsius to Fahrenheit
 func convertToFahrenheit(value float64) {
 	convertedValue := (value * 9 / 5) + 32
 	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
